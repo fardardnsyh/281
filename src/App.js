fdff789acb3d9e4b97806ca11ad038eb03b76865
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-25 18:50:24 +0700

    Files upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
